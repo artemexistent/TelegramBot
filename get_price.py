a443813040a57a241beb2c2de466523f302bf724
Артем Полтавський <54792047+artemexistent@users.noreply.github.com>
2020-10-20 21:12:14 +0300

    Delete bags

--- get_price.py
+++ get_price.py
@@ -14,11 +14,13 @@ duration = 1000  # Set Duration To 1000 ms == 1 second
 
 
 class Page(QWebEnginePage):
-
-    def __init__(self, url):
+    def __init__(self):
         self.app = QApplication(sys.argv)
         QWebEnginePage.__init__(self)
         self.html = ''
+
+
+    def new_url(self, url):
         self.loadFinished.connect(self._on_load_finished)
         self.load(QUrl(url))
         self.app.exec_()
@@ -31,6 +33,7 @@ class Page(QWebEnginePage):
         self.html = html_str
         self.app.quit()
 
+page = Page()
 
 def exact_url(url):
     index = url.find("B0")
@@ -41,23 +44,25 @@ def exact_url(url):
 
 
 def mainprogram(url):
-    # url = "https://www.amazon.com/Bose-QuietComfort-Wireless-Headphones-Cancelling/dp/B0756CYWWD/ref=sr_1_3?dchild=1&fst=as%3Aoff&pf_rd_i=16225009011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=82d03e2f-30e3-48bf-a811-d3d2a6628949&pf_rd_r=MGTEMX5Z31DSRNZVME50&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1486423355&refinements=p_n_shipping_option-bin%3A3242350011&rnid=493964&s=electronics&sr=1-3"
     exacturl = exact_url(url)  # main url to extract data
-    page = Page(exacturl)
+    page.new_url(exacturl)
     soup = bs.BeautifulSoup(page.html, 'html.parser')
     js_test = soup.find('span', id='priceblock_ourprice')
     if js_test is None:
         js_test = soup.find('span', id='priceblock_dealprice')
+    name = soup.find('span', id='productTitle')
     str = ""
     for line in js_test.stripped_strings:
         str = line
-
+    product = ''
+    for line in name.stripped_strings:
+        product = line
+    print(product)
         # convert to integer
     str = str.replace(", ", "")
-    # current_price = int(float(str))
     your_price = 600
     print(str)
-    return str
+    return (product, str)
 
 
 
